d54db6c0c52a4cd3da034c61535efa9361a81b75
Guichard² <jeancharles.g.guichard@gmail.com>
2025-09-08 14:44:51 -0300

    Implements database timezone

--- nest-api/src/database/typeorm.config.ts
+++ nest-api/src/database/typeorm.config.ts
@@ -13,6 +13,7 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   username: configService.get<string>('DB_USERNAME'),
   password: configService.get<string>('DB_PASSWORD'),
   database: configService.get<string>('DB_DATABASE'),
+  timezone: configService.get<string>('DB_TIMEZONE') ?? '-03:00',
   entities: [
     __dirname + '/../**/*.entity{.ts,.js}',
   ],
